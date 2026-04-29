9b259545471a26c4c6b3977e573bb7eaf6a9de1a
Owen-ais <owen.thomson14@icloud.com>
2026-04-28 21:04:49 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web_server.id
+}
 
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.web_server.public_dns
+}
+
+output "website_url" {
+  description = "Website URL"
+  value       = "http://${aws_instance.web_server.public_dns}"
+}
