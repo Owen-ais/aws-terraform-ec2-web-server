c49e2ea0ada7604236aea1cb9bc9ea91b89d9425
Owen-ais <owen.thomson14@icloud.com>
2026-04-28 21:05:47 -0400

    Update main.tf

--- Main.tf
+++ Main.tf
@@ -1,1 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ssm_parameter" "amazon_linux_2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow HTTP access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow HTTP from the internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-sg"
+    Project = var.project_name
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami                         = data.aws_ssm_parameter.amazon_linux_2023.value
+  instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  user_data                   = file("user-data.sh")
+
+  tags = {
+    Name    = var.project_name
+    Project = var.project_name
+  }
+}
