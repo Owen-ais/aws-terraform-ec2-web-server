adbcd7beee0b7a9aca24d772efd07d0a3ba110c6
Owen-ais <owen.thomson14@icloud.com>
2026-04-28 21:04:24 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "project_name" {
+  description = "Name used for tagging AWS resources"
+  type        = string
+  default     = "terraform-ec2-web-server"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
